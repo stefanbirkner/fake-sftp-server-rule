87a7f9bca7a23877788eb0ea6706719cbc82c502
Stefan Birkner <mail@stefan-birkner.de>
2016-02-18 00:00:00 +0000

    Add shortcut for uploading files.
    Files can be directly uploaded to the file system. This is more
    convenient for the user and faster than using a pure SFTP upload.

--- src/main/java/com/github/stefanbirkner/fakesftpserver/rule/FakeSftpServerRule.java
+++ src/main/java/com/github/stefanbirkner/fakesftpserver/rule/FakeSftpServerRule.java
@@ -9,9 +9,13 @@ import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.FileSystem;
+import java.nio.file.Path;
 
 import static com.github.marschall.memoryfilesystem.MemoryFileSystemBuilder.newLinux;
+import static java.nio.file.Files.createDirectories;
+import static java.nio.file.Files.write;
 import static java.util.Collections.singletonList;
 
 /**
@@ -31,9 +35,30 @@ import static java.util.Collections.singletonList;
  * arbitrary username and password. (The server accepts every combination of
  * username and password.) The port of the server is obtained by
  * {@link #getPort() sftpServer.getPort()}.
+ *
+ * <h2>Testing code that reads files</h2>
+ * <p>If you test code that reads files from an SFTP server then you need the
+ * server to provide these files. Fake SFTP Server Rule has a shortcut for
+ * uploading files to the server.
+ * <pre>
+ * &#064;Test
+ * public void testTextFile() {
+ *   {@link #putFile(String, String, Charset) sftpServer.putFile}("/directory/file.txt", "content of file", UTF_8);
+ *   //code that downloads the file
+ * }
+ *
+ * &#064;Test
+ * public void testBinaryFile() {
+ *   byte[] content = createContent();
+ *   {@link #putFile(String, byte[]) sftpServer.putFile}("/directory/file.bin", content);
+ *   //code that downloads the file
+ * }
+ * </pre>
  */
 public class FakeSftpServerRule implements TestRule {
     private static final int PORT = 23454;
+
+    private FileSystem fileSystem;
 
     /**
      * Returns the port of the SFTP server.
@@ -44,6 +69,36 @@ public class FakeSftpServerRule implements TestRule {
         return PORT;
     }
 
+    /**
+     * Put a text file on the SFTP folder. The file is available by the
+     * specified path.
+     * @param path the path to the file.
+     * @param content the files content.
+     * @param encoding the encoding of the file.
+     * @throws IOException if the file cannot be written.
+     */
+    public void putFile(String path, String content, Charset encoding)
+            throws IOException {
+        byte[] contentAsBytes = content.getBytes(encoding);
+        putFile(path, contentAsBytes);
+    }
+
+    /**
+     * Put a file on the SFTP folder. The file is available by the specified
+     * path.
+     * @param path the path to the file.
+     * @param content the files content.
+     * @throws IOException if the file cannot be written.
+     */
+    public void putFile(String path, byte[] content) throws IOException {
+        verifyThatTestIsRunning();
+        Path pathAsObject = fileSystem.getPath(path);
+        Path directory = pathAsObject.getParent();
+        if (directory != null && !directory.equals(pathAsObject.getRoot()))
+            createDirectories(directory);
+        write(pathAsObject, content);
+    }
+
     @Override
     public Statement apply(Statement base, Description description) {
         return new Statement() {
@@ -52,13 +107,16 @@ public class FakeSftpServerRule implements TestRule {
                 try (FileSystem fileSystem = createFileSystem();
                      SshServer server = startServer(fileSystem)) {
                     base.evaluate();
+                } finally {
+                    fileSystem = null;
                 }
             }
         };
     }
 
     private FileSystem createFileSystem() throws IOException {
-        return newLinux().build("FakeSftpServerRule@" + hashCode());
+        fileSystem = newLinux().build("FakeSftpServerRule@" + hashCode());
+        return fileSystem;
     }
 
     private SshServer startServer(FileSystem fileSystem) throws IOException {
@@ -70,5 +128,11 @@ public class FakeSftpServerRule implements TestRule {
         server.setFileSystemFactory((session) -> fileSystem);
         server.start();
         return server;
+    }
+
+    private void verifyThatTestIsRunning() {
+        if (fileSystem == null)
+            throw new IllegalStateException("Failed to upload file because"
+                + " test has not been started or is already finished.");
     }
 }
--- src/test/java/com/github/stefanbirkner/fakesftpserver/rule/FakeSftpServerRuleTest.java
+++ src/test/java/com/github/stefanbirkner/fakesftpserver/rule/FakeSftpServerRuleTest.java
@@ -1,14 +1,12 @@
 package com.github.stefanbirkner.fakesftpserver.rule;
 
 
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+import com.jcraft.jsch.*;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.ConnectException;
 
@@ -16,12 +14,14 @@ import static com.github.stefanbirkner.fakesftpserver.rule.Executor.executeTestT
 import static com.github.stefanbirkner.fakesftpserver.rule.Executor.executeTestWithRule;
 import static com.github.stefanbirkner.fishbowl.Fishbowl.exceptionThrownBy;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.io.IOUtils.toByteArray;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /* Wording according to the draft:
  * http://tools.ietf.org/html/draft-ietf-secsh-filexfer-13
  */
 public class FakeSftpServerRuleTest {
+    private static final byte[] DUMMY_CONTENT = new byte[]{1, 4, 2, 4, 2, 4};
     private static final JSch JSCH = new JSch();
     private static final int TIMEOUT = 100;
 
@@ -38,8 +38,7 @@ public class FakeSftpServerRuleTest {
         FakeSftpServerRule sftpServer = new FakeSftpServerRule();
         executeTestWithRule(() -> {
             Session session = connectToServer(sftpServer);
-            ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
-            channel.connect();
+            ChannelSftp channel = connectSftpChannel(session);
             channel.put(
                 new ByteArrayInputStream("dummy content".getBytes(UTF_8)),
                 "dummy_file.txt");
@@ -49,6 +48,101 @@ public class FakeSftpServerRuleTest {
             channel.disconnect();
             session.disconnect();
         }, sftpServer);
+    }
+
+    @Test
+    public void a_text_file_that_is_put_to_root_directory_via_the_rule_can_be_read_from_server() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        executeTestWithRule(() -> {
+            sftpServer.putFile("/dummy_file.txt", "dummy content with umlaut ü",
+                UTF_8);
+            byte[] file = downloadFile(sftpServer, "/dummy_file.txt");
+            assertThat(new String(file, UTF_8))
+                .isEqualTo("dummy content with umlaut ü");
+        }, sftpServer);
+    }
+
+    @Test
+    public void a_text_file_that_is_put_to_directory_via_the_rule_can_be_read_from_server() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        executeTestWithRule(() -> {
+            sftpServer.putFile("/dummy_directory/dummy_file.txt",
+                "dummy content with umlaut ü", UTF_8);
+            byte[] file = downloadFile(sftpServer,
+                "/dummy_directory/dummy_file.txt");
+            assertThat(new String(file, UTF_8))
+                .isEqualTo("dummy content with umlaut ü");
+        }, sftpServer);
+    }
+
+    @Test
+    public void a_text_file_cannot_be_put_before_the_test_is_started() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        Throwable exception = exceptionThrownBy(() ->
+            sftpServer.putFile("/dummy_file.txt", "dummy content", UTF_8));
+        assertThat(exception)
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("Failed to upload file because test has not been"
+                + " started or is already finished.");
+    }
+
+    @Test
+    public void a_text_file_cannot_be_put_after_the_test_is_finished() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        executeTestWithRule(() -> {
+        }, sftpServer);
+        Throwable exception = exceptionThrownBy(() ->
+            sftpServer.putFile("/dummy_file.txt", "dummy content", UTF_8));
+        assertThat(exception)
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("Failed to upload file because test has not been"
+                + " started or is already finished.");
+    }
+
+    @Test
+    public void a_binary_file_that_is_put_to_root_directory_via_the_rule_can_be_read_from_server() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        executeTestWithRule(() -> {
+            sftpServer.putFile("/dummy_file.bin", DUMMY_CONTENT);
+            byte[] file = downloadFile(sftpServer, "/dummy_file.bin");
+            assertThat(file).isEqualTo(DUMMY_CONTENT);
+        }, sftpServer);
+    }
+
+    @Test
+    public void a_binary_file_that_is_put_to_directory_via_the_rule_can_be_read_from_server() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        executeTestWithRule(() -> {
+            sftpServer.putFile("/dummy_directory/dummy_file.bin",
+                DUMMY_CONTENT);
+            byte[] file = downloadFile(sftpServer,
+                "/dummy_directory/dummy_file.bin");
+            assertThat(file).isEqualTo(DUMMY_CONTENT);
+        }, sftpServer);
+    }
+
+    @Test
+    public void a_binary_file_cannot_be_put_before_the_test_is_started() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        Throwable exception = exceptionThrownBy(() ->
+            sftpServer.putFile("/dummy_file.bin", DUMMY_CONTENT));
+        assertThat(exception)
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("Failed to upload file because test has not been"
+                + " started or is already finished.");
+    }
+
+    @Test
+    public void a_binary_file_cannot_be_put_after_the_test_is_finished() {
+        FakeSftpServerRule sftpServer = new FakeSftpServerRule();
+        executeTestWithRule(() -> {
+        }, sftpServer);
+        Throwable exception = exceptionThrownBy(() ->
+            sftpServer.putFile("/dummy_file.bin", DUMMY_CONTENT));
+        assertThat(exception)
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("Failed to upload file because test has not been"
+                + " started or is already finished.");
     }
 
     @Test
@@ -73,12 +167,32 @@ public class FakeSftpServerRuleTest {
     }
 
     private Session connectToServer(FakeSftpServerRule sftpServer)
-            throws JSchException {
+        throws JSchException {
         Session session = JSCH.getSession(
             "dummy user", "127.0.0.1", sftpServer.getPort());
         session.setConfig("StrictHostKeyChecking", "no");
         session.setPassword("dummy password");
         session.connect(TIMEOUT);
         return session;
+    }
+
+    private ChannelSftp connectSftpChannel(Session session)
+            throws JSchException {
+        ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
+        channel.connect();
+        return channel;
+    }
+
+    private byte[] downloadFile(FakeSftpServerRule server, String path)
+            throws JSchException, SftpException, IOException {
+        Session session = connectToServer(server);
+        ChannelSftp channel = connectSftpChannel(session);
+        try {
+            InputStream is = channel.get(path);
+            return toByteArray(is);
+        } finally {
+            channel.disconnect();
+            session.disconnect();
+        }
     }
 }
